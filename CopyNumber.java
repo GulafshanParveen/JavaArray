b1594d33b58c7c96503ae04b536e6722cff320b7
Gulafshan Parveen <64768393+GulafshanParveen@users.noreply.github.com>
2020-05-29 07:49:05 +0530

    Copy number of given array

---
+++ CopyNumber.java
@@ -0,0 +1,9 @@
+class CopyNumber{
+	public static void main(String[] args){
+		int[] num={12,13,11,43,55};
+		int[] number=num;
+		for(int items:number){
+		System.out.print(items+ ",");
+		}
+	}
+}
