639b28af305ca756fc5a3d054da4c595941ca7f4
Gulafshan Parveen <64768393+GulafshanParveen@users.noreply.github.com>
2020-05-29 06:16:15 +0530

    Array Intialization

---
+++ ArrayInitialize.java
@@ -0,0 +1,8 @@
+class ArrayInitialize{
+	public static void main(String[] args){
+		int[] num={12,4,6,7,8};
+		for(int i=0;i<=5;i++){
+			System.out.println("Display given array "+ i +"=" +num[i]);
+		}
+	}
+}
